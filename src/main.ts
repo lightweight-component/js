1da0daa280ed0bcaec3b90ff33331fbd966335b2
sp42@qq.com <sp42@qq.com>
2024-07-01 22:50:19 +0800

    ci

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import Vue from 'vue'
-import widget from './components/api-selector/index.vue';
+import apiSelector from './api-selector/index.vue';
+import apiHelper from './api-helper/index.vue';
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
 
 Vue.config.productionTip = false;
 Vue.use(ViewUI);
 
-new Vue({render: h => h(widget)}).$mount('#app');
+const apiSelectorFn = h => h(apiSelector, {
+    props: {
+        apiRoot: 'foo'
+    }
+});
+
+const apiHelperFn = h => h(apiHelper, {
+    props: {
+        apiRoot: 'foo'
+    }
+});
+
+new Vue({ render: apiHelperFn }).$mount('#app');
