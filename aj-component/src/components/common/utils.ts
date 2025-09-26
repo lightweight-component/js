8a94c86e964f5ea9f5df216e6c68e7feeb78b462
sp42 <sp42@qq.com>
2025-09-27 00:27:56 +0800

    ci

--- aj-component/src/components/common/utils.ts
+++ aj-component/src/components/common/utils.ts
@@ -33,3 +33,60 @@ export function copyToClipboard(text: string): void {
         document.body.removeChild(textarea);
     }
 }
+
+/**
+ * 通用的打开下载对话框方法，没有测试过具体兼容性
+ * https://www.cnblogs.com/liuxianan/p/js-download.html
+ * 
+ * ref 这应该是你见过的最全前端下载总结 https://juejin.cn/post/6844903763359039501
+ * 
+ * @param url 下载地址，也可以是一个blob对象，必选
+ * @param saveName 保存文件名，可选
+ */
+export function openDownloadDialog(url: string | Blob, saveName: string): void {
+    if (typeof url == 'object' && url instanceof Blob)
+        url = URL.createObjectURL(url); // 创建blob地址
+
+    const aLink: HTMLAnchorElement = document.createElement('a');
+    aLink.href = url;
+    aLink.download = saveName || ''; // HTML5新增的属性，指定保存文件名，可以不要后缀，注意，file:///模式下不会生效
+
+    let event;
+    if (window.MouseEvent)
+        event = new MouseEvent('click');
+    else {
+        event = document.createEvent('MouseEvents');
+        event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    }
+
+    aLink.dispatchEvent(event);
+}
+
+export function myHTMLInclude(): void {
+    let a: Element, file: string | null, xhttp: XMLHttpRequest;
+    const z = document.getElementsByTagName("*");
+
+    for (let i = 0; i < z.length; i++) {
+        if (z[i].getAttribute("w3-include-html")) {
+            a = z[i].cloneNode(false) as Element;
+            file = z[i].getAttribute("w3-include-html");
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (xhttp.readyState == 4 && xhttp.status == 200) {
+                    a.removeAttribute("w3-include-html");
+                    a.innerHTML = xhttp.responseText;
+                    // @ts-ignore
+                    z[i].parentNode.replaceChild(a, z[i]);
+                    myHTMLInclude();
+                }
+            }
+
+            if (file) {
+                xhttp.open("GET", file, false);
+                xhttp.send();
+            }
+
+            return;
+        }
+    }
+}
